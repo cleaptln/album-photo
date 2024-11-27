66fe208b82b8188bb7c0a6031b3065f1295bc90d
Antoineq25 <antoine.qgn2@posteo.net>
2024-11-27 17:46:19 +0100

    css -> header & page detail albums

--- resources/views/detailsAlbum.blade.php
+++ resources/views/detailsAlbum.blade.php
@@ -2,24 +2,57 @@
 
 @section("contenu")
 
-page details album
-
 <label for="trie"></label>
 <select name="trie" id="trie">
     <option value="notes">Note</option>
     <option value="titres">Titre</option>
 </select>
 
+    <!-- Début Bouton tri -->
+<div class="tri">
+<div class="circle-button" id="circleButton">
+<span class="plus-sign">Tri <i class='bx bx-filter-alt'></i></span>
+        <div class="radio-list" id="radioList">
+            <label class="radio-item">
+                <input type="radio" name="option" value="1"> Titre
+            </label>
+            <label class="radio-item">
+                <input type="radio" name="option" value="2"> Note
+            </label>
+        </div>
+    </div>
+</div>
 
-<h1> {{$album->titre}} </h1>
+    <script>
+        const circleButton = document.getElementById("circleButton");
+        const radioList = document.getElementById("radioList");
+
+        // Activé l'expension au clique du bouton
+        circleButton.addEventListener("click", (event) => {
+            event.stopPropagation(); // Prevent event bubbling to body
+            circleButton.classList.toggle("expanded");
+        });
+
+        // Ferme la fenêtre si on clique en dehors
+        document.body.addEventListener("click", () => {
+            circleButton.classList.remove("expanded");
+        });
+    </script>
+
+    <!-- Fin Bouton tri -->
+
+
+<h1 class="titre_album"> {{$album->titre}} </h1>
     @foreach($photo as $p)
+    <div class="card">
        <h2> {{$p->titre}}</h2>
        <img src="{{$p->url}}">
        
        @foreach($tag[$p->id] as $t)
        <a href="{{route('tag',['tag'=>$t->nom])}}">#{{$t->nom}}</a>
        @endforeach
-       
+    </div>
     @endforeach
 
 @endsection
+
