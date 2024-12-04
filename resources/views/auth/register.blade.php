ad1a7c06ae77d2b9d046ab91bf17a0b3e6a437b4
Antoineq25 <antoine.qgn2@posteo.net>
2024-12-04 10:33:57 +0100

    CSS -> Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,16 +2,18 @@
 
 @section('contenu')
 
+<h1 id="titre_register"> Créer un compte </h1>
 
+<div class="register">
 <form action="{{route('register')}}" method="post">
     @csrf
-    <input type="text" name="name" required placeholder="Name" /><br />
-    <input type="email" name="email" required placeholder="Email" /><br />
-    <input type="password" name="password" required placeholder="password" /><br />
-    <input type="password" name="password_confirmation" required placeholder="password" /><br />
+    <input type="text" name="name" required placeholder="Nom" /><br />
+    <input type="email" name="email" required placeholder="Adresse email" /><br />
+    <input type="password" name="password" required placeholder="Mot de passe" /><br />
+    <input type="password" name="password_confirmation" required placeholder="Confirmer le mot de passe" /><br />
     <input type="submit" /><br />
 </form>
-Déjà un compte  ? <a href="{{route('login')}}">Connectez-vous</a>
-
+<a href="{{route('login')}}">J'ai déjà un compte</a>
+</div>
 
 @endsection
